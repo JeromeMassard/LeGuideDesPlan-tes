20d2775104c5976d5e340c51063c613e8e609f06
jmddu_000 <jmddu_000@PC_JEROME>
2016-05-31 18:35:34 +0200

    Modification

--- GuideDesPlanètesDuPetitVoyager/ClassLibrary1/DelegateCommande.cs
+++ GuideDesPlanètesDuPetitVoyager/ClassLibrary1/DelegateCommande.cs
@@ -11,6 +11,7 @@ namespace Library
     {
         private readonly Action<object> _execute;
         private readonly Predicate<object> _canExecute;
+
         public event EventHandler CanExecuteChanged;
 
         public bool CanExecute(object parameter)
